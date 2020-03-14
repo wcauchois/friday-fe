62f3f8547a1d07b78ec5a2e8e55744c3ce6f8fee
Bill Cauchois <bill@tryfi.com>
2020-03-14 15:17:53 -0400

    implement da app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,101 @@
-import React from 'react';
-import './App.css';
-import 'antd/dist/antd.css';
-import { DatePicker, message, Layout, Button, Row } from 'antd';
+import React from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+import { Typography, List, Form, Input, Button } from "antd";
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider, useQuery, useMutation } from "@apollo/react-hooks";
+
+const graphqlEndpoint =
+  process.env.REACT_APP_GRAPHQL_ENDPOINT ?? "http://localhost:8000/graphql";
+const client = new ApolloClient({
+  uri: graphqlEndpoint
+});
 
 // https://medium.com/@ss_81611/ant-design-the-best-react-ui-library-ive-ever-used-3e0a48ab24c3
 
+const ALL_POSTS = gql`
+  {
+    allPosts {
+      id
+      body
+    }
+  }
+`;
+
+function PostList() {
+  const { loading, error, data } = useQuery(ALL_POSTS);
+  if (loading) {
+    return null;
+  }
+  if (error) {
+    return <div>There was an error: {error.message}</div>;
+  }
+
+  console.log("all posts are", data.allPosts);
+  return (
+    <List
+      header={<div>All Posts</div>}
+      bordered
+      dataSource={data.allPosts}
+      renderItem={(post: any) => <List.Item>{post.body}</List.Item>}
+    />
+  );
+}
+
+const ADD_POST = gql`
+  mutation AddPost($input: PostInput!) {
+    addPost(input: $input) {
+      id
+    }
+  }
+`;
+
+function AddPostForm() {
+  const [addPost] = useMutation(ADD_POST, {
+    refetchQueries: [{ query: ALL_POSTS }]
+  });
+
+  const onFinish = async (values: any) => {
+    await addPost({
+      variables: {
+        input: {
+          body: values.body
+        }
+      }
+    });
+  };
+
+  return (
+    <Form
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ body: "" }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Body"
+        name="body"
+        rules={[{ required: true, message: `Enter a post body` }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <Row>
-        Hi
-      </Row>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <PostList />
+        <AddPostForm />
+      </div>
+    </ApolloProvider>
   );
 }
 
